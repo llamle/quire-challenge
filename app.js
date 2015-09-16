75da735b408d688719bb676376770aa6c4d07840
Leon Lamle <llamle@gmail.com>
2015-09-15 21:02:14 -0400

    added random error messages to nudge the user towards the right answer

--- app.js
+++ app.js
@@ -14,12 +14,26 @@ app.controller('MainCtrl', ['$scope', function($scope) {
     {title: 'All of the above', value: 'no'}
   ];
 
+
   $scope.verify = function() {
     if ($scope.isCorrect.value === 'yes') {
       alert('Awesome, this is correct!');
-    } else {
-      alert('We should give this another try...')
+    } else if ($scope.isCorrect.value === 'not sure') {
+      alert('“Do. Or do not. There is no try.” – Yoda');
     }
-  }
+    else {
+      var randomNumber = Math.floor(Math.random()*$scope.incorrect.length);
+
+      alert($scope.incorrect[randomNumber]);
+    }
+  };
+
+  $scope.incorrect = [
+    'When Life Gives You Questions, Google has Answers...',
+    'If at first you don\'t succeed; call it version 1.0...',
+    'Alcohol & calculus don\'t mix. Never drink & derive...',
+    '“It’s a trap!” – Admiral Ackbar',
+    '“Never tell me the odds.” – Han Solo',
+  ];
 
 }]);
